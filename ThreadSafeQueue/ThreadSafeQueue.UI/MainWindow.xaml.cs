4af011d78366fecfcf9f479ee3390dd43de1c91b
Platonov Eugene <platonov_eugene@mail.ru>
2016-05-15 18:54:39 +0300

    Выполнена реализация загрузки тестовых сценариев.

--- ThreadSafeQueue/ThreadSafeQueue.UI/MainWindow.xaml.cs
+++ ThreadSafeQueue/ThreadSafeQueue.UI/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace ThreadSafeQueue.UI
             InitializeComponent();
         }
 
-        private void SwitchingOnAndOffStreams(object sender, RoutedEventArgs e)
+        private void SwitchingOnAndOffThreads(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
             bool isEnabledThread = (button.Content.ToString() == "Включен");
@@ -70,17 +70,18 @@ namespace ThreadSafeQueue.UI
         private void RunThreads(object sender, RoutedEventArgs e)
         {
             if (!labelHeaderThreadA.IsEnabled && !labelHeaderThreadB.IsEnabled && !labelHeaderThreadC.IsEnabled && !labelHeaderThreadD.IsEnabled)
-                MessageBox.Show("Для успешного запуска потоков, необходимо выполнить включение в работу хотя-бы одного из потоков.", "Ошибка при запуске потоков", MessageBoxButton.OK, MessageBoxImage.Warning);
+                MessageBox.Show("Для успешного запуска потоков необходимо выполнить включение в работу как минимум одного из потоков.", "Ошибка при запуске потоков", MessageBoxButton.OK, MessageBoxImage.Warning);
             else
             {
 
             }
         }
 
-        private void EnableAllThreads()
+        private void EnableAndCleanAllThreads()
         {
             labelHeaderThreadA.IsEnabled = true;
             richtextboxOperationInThreadA.IsEnabled = true;
+            richtextboxOperationInThreadA.Document.Blocks.Clear();
             labelIntervalRunningOperationsInThreadA.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadA.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadA.SelectedIndex = -1;
@@ -89,6 +90,7 @@ namespace ThreadSafeQueue.UI
 
             labelHeaderThreadB.IsEnabled = true;
             richtextboxOperationInThreadB.IsEnabled = true;
+            richtextboxOperationInThreadB.Document.Blocks.Clear();
             labelIntervalRunningOperationsInThreadB.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadB.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadB.SelectedIndex = -1;
@@ -97,6 +99,7 @@ namespace ThreadSafeQueue.UI
 
             labelHeaderThreadC.IsEnabled = true;
             richtextboxOperationInThreadC.IsEnabled = true;
+            richtextboxOperationInThreadC.Document.Blocks.Clear();
             labelIntervalRunningOperationsInThreadC.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadC.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadC.SelectedIndex = -1;
@@ -105,6 +108,7 @@ namespace ThreadSafeQueue.UI
 
             labelHeaderThreadD.IsEnabled = true;
             richtextboxOperationInThreadD.IsEnabled = true;
+            richtextboxOperationInThreadD.Document.Blocks.Clear();
             labelIntervalRunningOperationsInThreadD.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadD.IsEnabled = true;
             comboboxIntervalRunningOperationsInThreadD.SelectedIndex = -1;
@@ -112,83 +116,168 @@ namespace ThreadSafeQueue.UI
             buttonSwitchingOnAndOffThreadD.Foreground = Brushes.Black;
         }
 
-        private void CleaningOperationsInThreads()
+        private void FormattingDefinedOperations(RichTextBox richTextBox)
         {
-            //richtextboxOperationInThreadA.Document
-        }
-
-        private void FormattingDefinedOperations()
-        {
-            FlowDocument flowDocument = richtextboxOperationInThreadA.Document;
-            foreach (Paragraph paragraph in richtextboxOperationInThreadA.Document.Blocks)
-                if (new TextRange(paragraph.ContentStart, paragraph.ContentEnd).Text.Contains("Push"))
-                    paragraph.Foreground = Brushes.Green;
-                else
-                    paragraph.Foreground = Brushes.Blue;
-
-            flowDocument = richtextboxOperationInThreadB.Document;
-            foreach (Paragraph paragraph in richtextboxOperationInThreadB.Document.Blocks)
-                if (new TextRange(paragraph.ContentStart, paragraph.ContentEnd).Text.Contains("Push"))
-                    paragraph.Foreground = Brushes.Green;
-                else
-                    paragraph.Foreground = Brushes.Blue;
-
-            flowDocument = richtextboxOperationInThreadC.Document;
-            foreach (Paragraph paragraph in richtextboxOperationInThreadC.Document.Blocks)
-                if (new TextRange(paragraph.ContentStart, paragraph.ContentEnd).Text.Contains("Push"))
-                    paragraph.Foreground = Brushes.Green;
-                else
-                    paragraph.Foreground = Brushes.Blue;
-
-            flowDocument = richtextboxOperationInThreadD.Document;
-            foreach (Paragraph paragraph in richtextboxOperationInThreadD.Document.Blocks)
+            foreach (Paragraph paragraph in richTextBox.Document.Blocks)
                 if (new TextRange(paragraph.ContentStart, paragraph.ContentEnd).Text.Contains("Push"))
                     paragraph.Foreground = Brushes.Green;
                 else
                     paragraph.Foreground = Brushes.Blue;
         }
 
-        private void LoadStringOperations(object sender, RoutedEventArgs e)
+        private void FormattingDefinedOperationsInThreads()
         {
+            FormattingDefinedOperations(richtextboxOperationInThreadA);
+            FormattingDefinedOperations(richtextboxOperationInThreadB);
+            FormattingDefinedOperations(richtextboxOperationInThreadC);
+            FormattingDefinedOperations(richtextboxOperationInThreadD);
         }
 
-        private void LoadIntegerOperations(object sender, RoutedEventArgs e)
+        private void LoadStringOperationsInThreads(object sender, RoutedEventArgs e)
         {
-            EnableAllThreads();
+            Cursor = Cursors.Wait;
+            EnableAndCleanAllThreads();
 
-            richtextboxOperationInThreadA.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadA.AppendText("string Pop()"  + "\n\n");
+            richtextboxOperationInThreadA.AppendText("Push(\"ABC\")" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push(\"DEF\")" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push(\"GHI\")" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push(\"JKL\")" + "\n");
+            richtextboxOperationInThreadA.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadA.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadA.AppendText("Push(\"MNO\")");
+            comboboxIntervalRunningOperationsInThreadA.SelectedIndex = 0;
+
+            richtextboxOperationInThreadB.AppendText("Push(\"PQR\")" + "\n\n");
+            richtextboxOperationInThreadB.AppendText("Push(\"STU\")" + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(\"VWX\")" + "\n");
+            richtextboxOperationInThreadB.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(\"YZA\")" + "\n");
+            richtextboxOperationInThreadB.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(\"AGM\")" + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(\"BHN\")");
+            comboboxIntervalRunningOperationsInThreadB.SelectedIndex = 1;
+
+            richtextboxOperationInThreadC.AppendText("string Pop()"  + "\n\n");
+            richtextboxOperationInThreadC.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadC.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(\"CIO\")" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(\"DJP\")" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(\"EKQ\")" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(\"FLR\")" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(\"MSX\")");
+            comboboxIntervalRunningOperationsInThreadC.SelectedIndex = 1;
+
+            richtextboxOperationInThreadD.AppendText("Push(\"NTY\")" + "\n\n");
+            richtextboxOperationInThreadD.AppendText("Push(\"OUZ\")" + "\n");
+            richtextboxOperationInThreadD.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadD.AppendText("Push(\"PVW\")" + "\n");
+            richtextboxOperationInThreadD.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadD.AppendText("Push(\"QWR\")" + "\n");
+            richtextboxOperationInThreadD.AppendText("string Pop()"  + "\n");
+            richtextboxOperationInThreadD.AppendText("Push(\"XYZ\")");
+            comboboxIntervalRunningOperationsInThreadD.SelectedIndex = 0;
+
+            FormattingDefinedOperationsInThreads();
+            Cursor = Cursors.Arrow;
+        }
+
+        private void LoadIntegerOperationsInThreads(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.Wait;
+            EnableAndCleanAllThreads();
+
+            richtextboxOperationInThreadA.AppendText("Int32 Pop()" + "\n\n");
             richtextboxOperationInThreadA.AppendText("Push(-64)"   + "\n");
             richtextboxOperationInThreadA.AppendText("Push(378)"   + "\n");
             richtextboxOperationInThreadA.AppendText("Int32 Pop()" + "\n");
             richtextboxOperationInThreadA.AppendText("Push(495)"   + "\n");
+            richtextboxOperationInThreadA.AppendText("Push(560)"   + "\n");
+            richtextboxOperationInThreadA.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push(-91)");
             comboboxIntervalRunningOperationsInThreadA.SelectedIndex = 0;
 
-            richtextboxOperationInThreadB.AppendText("Push(132)"   + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(132)"   + "\n\n");
             richtextboxOperationInThreadB.AppendText("Int32 Pop()" + "\n");
             richtextboxOperationInThreadB.AppendText("Push(256)"   + "\n");
             richtextboxOperationInThreadB.AppendText("Int32 Pop()" + "\n");
-            richtextboxOperationInThreadB.AppendText("Push(-98)"   + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(-88)"   + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(804)"   + "\n");
+            richtextboxOperationInThreadB.AppendText("Push(-70)"   + "\n");
+            richtextboxOperationInThreadB.AppendText("Int32 Pop()");
             comboboxIntervalRunningOperationsInThreadB.SelectedIndex = 1;
 
-            richtextboxOperationInThreadC.AppendText("Push(725)"   + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(725)"   + "\n\n");
             richtextboxOperationInThreadC.AppendText("Int32 Pop()" + "\n");
             richtextboxOperationInThreadC.AppendText("Push(953)"   + "\n");
             richtextboxOperationInThreadC.AppendText("Int32 Pop()" + "\n");
             richtextboxOperationInThreadC.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(-48)"   + "\n");
+            richtextboxOperationInThreadC.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push(743)");
             comboboxIntervalRunningOperationsInThreadC.SelectedIndex = 2;
 
-            richtextboxOperationInThreadD.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("Int32 Pop()" + "\n\n");
             richtextboxOperationInThreadD.AppendText("Int32 Pop()" + "\n");
             richtextboxOperationInThreadD.AppendText("Push(-12)"   + "\n");
             richtextboxOperationInThreadD.AppendText("Int32 Pop()" + "\n");
             richtextboxOperationInThreadD.AppendText("Push(-39)"   + "\n");
+            richtextboxOperationInThreadD.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("Int32 Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("Push(154)");
             comboboxIntervalRunningOperationsInThreadD.SelectedIndex = 0;
 
-            FormattingDefinedOperations();
+            FormattingDefinedOperationsInThreads();
+            Cursor = Cursors.Arrow;
         }
 
-        private void LoadCharacterOperations(object sender, RoutedEventArgs e)
+        private void LoadCharacterOperationsInThreads(object sender, RoutedEventArgs e)
         {
+            Cursor = Cursors.Wait;
+            EnableAndCleanAllThreads();
+
+            richtextboxOperationInThreadA.AppendText("char Pop()" + "\n\n");
+            richtextboxOperationInThreadA.AppendText("Push('А')" + "\n");
+            richtextboxOperationInThreadA.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push('Б')" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push('')" + "\n");
+            richtextboxOperationInThreadA.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadA.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadA.AppendText("Push('В')");
+            comboboxIntervalRunningOperationsInThreadA.SelectedIndex = 4;
+
+            richtextboxOperationInThreadB.AppendText("Push('Г')" + "\n\n");
+            richtextboxOperationInThreadB.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadB.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadB.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadB.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadB.AppendText("Push('Д')" + "\n");
+            richtextboxOperationInThreadB.AppendText("Push('Е')" + "\n");
+            richtextboxOperationInThreadB.AppendText("Push('Ё')");
+            comboboxIntervalRunningOperationsInThreadB.SelectedIndex = 2;
+        
+            richtextboxOperationInThreadC.AppendText("Push('Ж')" + "\n\n");
+            richtextboxOperationInThreadC.AppendText("Push('З')" + "\n");
+            richtextboxOperationInThreadC.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push('И')" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push('Й')" + "\n");
+            richtextboxOperationInThreadC.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadC.AppendText("Push('К')" + "\n");
+            richtextboxOperationInThreadC.AppendText("char Pop()");
+            comboboxIntervalRunningOperationsInThreadC.SelectedIndex = 4;
+
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n\n");
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("char Pop()" + "\n");
+            richtextboxOperationInThreadD.AppendText("Push('Л')");
+            comboboxIntervalRunningOperationsInThreadD.SelectedIndex = 1;
+
+            FormattingDefinedOperationsInThreads();
+            Cursor = Cursors.Arrow;
         }
     }
 }
